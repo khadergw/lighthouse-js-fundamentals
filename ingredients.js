8e91ef1f777536189302e6e1995f3aebbba524ba
GHadeer Khader <qods_a@yahoo.com>
2021-10-04 23:44:09 +0000

    Add more conditional examples

---
+++ ingredients.js
@@ -0,0 +1,10 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0;
+
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++; 
+} 
+for (var i = ingredients.length - 1; i >= 0; i--) {
+    console.log(ingredients[i]);
+}
